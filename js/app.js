fcec4f9187eafdeb971e90fc1361e7efb86d4f07
janeosaur <janewie@gmail.com>
2017-08-25 20:09:27 -0700

    adds street view overlay to google map'

--- js/app.js
+++ js/app.js
@@ -1,28 +1,27 @@
 $(document).ready(function() {
 
   $(window).scroll(function() {
-
     if ($(window).scrollTop() > 100) {
-        $('header').addClass('sticky');
+      $('header').addClass('sticky'); // shows navbar when scrolled down
     } else {
-        $('header').removeClass('sticky');
+      $('header').removeClass('sticky');
     }
   });
 
   // Mobile Navigation
   $('.mobile-toggle').click(function() {
-      if ($('header').hasClass('open-nav')) {
-          $('header').removeClass('open-nav');
-      } else {
-          $('header').addClass('open-nav');
-      }
+    if ($('header').hasClass('open-nav')) {
+      $('header').removeClass('open-nav');
+    } else {
+      $('header').addClass('open-nav');
+    }
   });
 
-  $('.main_h li a').click(function() {
-      if ($('header').hasClass('open-nav')) {
-          $('.navigation').removeClass('open-nav');
-          $('header').removeClass('open-nav');
-      }
+  $('header li a').click(function() {
+    if ($('header').hasClass('open-nav')) {
+      $('.navigation').removeClass('open-nav');
+      $('header').removeClass('open-nav');
+    }
   });
 
   // navigation scroll
@@ -45,6 +44,7 @@ $(document).ready(function() {
   });
 
   function initMap() {
+    // var panorama;
     var center = {lat: 37.5185, lng: -77.3518};
     var map = new google.maps.Map(document.getElementById('map'), {
       center: center,
@@ -52,17 +52,28 @@ $(document).ready(function() {
     });
     var marker = new google.maps.Marker({
       position: center,
-      map: map
+      map: map,
+      title: 'SiStar Seafood',
+      icon: ''
     })
-    var panorama = new google.maps.StreetViewPanorama(
-      document.getElementById('street'), {
-        position: center,
-        pov: {
-          heading: 34,
-          pitch: 10
-        }
-      });
-    map.setStreetView(panorama);
+    var panorama = map.getStreetView();
+    panorama.setPosition(center);
+    panorama.setPov(/** @type {google.maps.StreetViewPov} */({
+      heading: 70,
+      pitch: 0
+    }));
+
+    function toggleStreetView() {
+      var toggle = panorama.getVisible();
+      if (toggle == false) {
+        panorama.setVisible(true);
+      } else {
+        panorama.setVisible(false);
+      }
+    }
+    $('#floating-panel').click(function(e) {
+      toggleStreetView();
+    })
   }
 
   initMap();
